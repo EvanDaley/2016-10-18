4430322cff083557d5e7309376b7a98ff8b2a3a2
Evan <daley.evan@gmail.com>
2016-10-18 18:42:38 -0700

    Wrap up today's game

---
+++ 2016-10-18-CardboardVR4/Assets/UtilityScripts/Navmesh/SimpleNavmeshActor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleNavmeshActor : MonoBehaviour {
+
+	public Transform targetObject;
+
+	private NavMeshAgent navMeshAgent;
+
+	void Start () 
+	{
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+
+		navMeshAgent.SetDestination (targetObject.position);
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "goal1")
+		{
+			navMeshAgent.SetDestination(GameObject.Find ("goal2").transform.position);
+
+		} else if (other.gameObject.name == "goal2")
+		{
+			navMeshAgent.SetDestination(GameObject.Find ("goal3").transform.position);
+		}else if (other.gameObject.name == "goal3")
+		{
+			navMeshAgent.SetDestination(GameObject.Find ("goal4").transform.position);
+		}else if (other.gameObject.name == "goal4")
+		{
+			navMeshAgent.SetDestination(GameObject.Find ("goal1").transform.position);
+		}
+	}
+}
